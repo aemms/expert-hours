65834e2932828a847c96dcfbd865506a349ce6c5
Alex Emms <me@alexemms.com>
2016-05-28 23:36:13 +0100

    changed build process

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: `
-  					<h1>Expert Hours</h1>
-  					<p>To do: create model of project</p>
-  					`
+  templateUrl: 'dist/app.component.html'
 })
-export class AppComponent { }
+
+export class AppComponent {
+	project: Project;
+	projects: Project[];
+
+	constructor() {
+		this.projects = [{
+			id: 0,
+			name: "javascript",
+			hours: 10
+		}, {
+			id: 1,
+			name: "testing",
+			hours: 20
+		}];
+	}
+
+	addHours(project: Project, hours: number){		
+		console.log(project.hours + " \n " + hours);
+		this.projects[project.id].hours += hours;
+	}
+}
+
+export class Project {
+	id: number;
+	name: string;
+	hours: number;
+}
