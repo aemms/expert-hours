203103e6cfe9bbd6be5c7ce52c4e82ebde22348c
Alex Emms <me@alexemms.com>
2016-05-30 23:05:57 +0100

    sass to project with gulp

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,11 +11,9 @@ export class AppComponent {
 
 	constructor() {
 		this.projects = [{
-			id: 0,
-			name: "javascript",
+			name: "angular",
 			hours: 10
 		}, {
-			id: 1,
 			name: "testing",
 			hours: 20
 		}];
@@ -25,14 +23,17 @@ export class AppComponent {
 		this.selectedProject = project;
 	}
 
-	addHours(project: Project, hours: number){		
-		this.selectedProject.hours += +hours;
-		console.log(this.selectedProject.hours);
+	addHours(project: Project, hours){
+		this.selectedProject.hours += +hours.value;
+		hours.value = '';
+	}
+
+	addProject(name: string, hours: number){
+		this.projects.push({ name, hours });
 	}
 }
 
 export class Project {
-	id: number;
 	name: string;
 	hours: number;
 }
