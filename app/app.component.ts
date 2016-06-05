57625dc44fa74af6d01c2d28f9d9ec9729e2a9aa
Alex Emms <me@alexemms.com>
2016-06-05 23:03:52 +0100

    start mouseover thing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { NgForm } from 		'@angular/common';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  host: {
+  	'(mouseenter)': 'onMouseEnter()',
+  	'(mouseleave)': 'onMouseLeave()'
+  }
 })
 
 export class AppComponent {
 	selectedProject: Project;
 	public projects = PROJECTS;
+	private el: HTMLElement;
+
+	contructor(el: ElementRef){
+		this.el = el.nativeElement;
+	}
 
 	model = new Project("test", 150);
 
@@ -23,6 +32,14 @@ export class AppComponent {
 
 	addProject(project: any): void{
 		this.projects.push({name: project.name, hours: +project.hours});
+	}
+
+	onMouseEnter(){
+		//switch inners of li to show add project
+	}
+
+	onMouseLeave(){
+		//show normal top bit
 	}
 }
 
